c6fb5a119559ceac657a158ca24d3689dafbe99f
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-06-07 21:25:56 -0300

    feat: create main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import usePersistedState from './hooks/usePersistedState.js';
+
 function App() {
+    const [token, setToken] = usePersistedState('token', null);
+
     return (
-        <>
-            <h1>Hello, world!</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<h1>tela inicial</h1>} />
+            <Route path="/login" element={<h1>tela de login</h1>} />
+            <Route path="/register" element={<h1>tela de registro</h1>} />
+            <Route path="/ranking" element={<h1>tela de ranking</h1>} />
+        </Routes>
     );
 }
 
