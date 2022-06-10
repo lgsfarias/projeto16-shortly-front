b064fdb7af075623e568b03f782d5cfd557b9fab
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-06-09 22:20:33 -0300

    fix: font family

--- src/components/UrlContainer/style.js
+++ src/components/UrlContainer/style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const UrlContainer = styled.div`
+    font-family: 'Lexend Deca', sans-serif;
     display: flex;
     justify-content: space-between;
     align-items: center;
